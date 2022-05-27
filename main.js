0ca33aca0cc524c6d9be961c6898e3ed042cb576
sebastiano minotti <seba.minotti@gmail.com>
2022-05-27 17:29:52 +0200

    ultimato
    chiamata axios terminata

---
+++ main.js
@@ -0,0 +1,27 @@
+const app =  new Vue({
+  el:'#root',
+
+  data:{
+      mailList:[],
+
+  },
+ 
+  mounted(){
+
+
+         for(let i = 0; i < 10; i++ ){
+
+             axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((mail)=>{
+                    this.mailList.push(mail.data.response)
+            })
+         }
+            
+         
+    
+
+  }
+
+
+})
